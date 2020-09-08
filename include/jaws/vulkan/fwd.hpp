e90308efbabdbefabfa3ede5b999efa1b1556f72
Jens <jens@weggemann.de>
2020-09-08 08:00:26 +0200

    Forward-declared resource classes and pools.

--- include/jaws/vulkan/fwd.hpp
+++ include/jaws/vulkan/fwd.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "jaws/util/ref_ptr.hpp"
+#include "jaws/util/Pool.hpp"
 
 namespace jaws::vulkan {
 
@@ -10,5 +10,11 @@ class WindowContext;
 class ShaderSystem;
 struct ShaderCreateInfo;
 class Shader;
+
+class Buffer;
+class Image;
+
+using BufferPool = jaws::util::Pool<Buffer, uint64_t, 16, 32>;
+using ImagePool = jaws::util::Pool<Image, uint64_t, 16, 32>;
 
 }
