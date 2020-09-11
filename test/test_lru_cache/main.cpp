03a5926a725f717a99941a16fe2458701d4dad70
Jens <jens@weggemann.de>
2020-09-11 14:44:03 +0200

    Adapted LruCache test to latest changes.

--- test/test_lru_cache/main.cpp
+++ test/test_lru_cache/main.cpp
@@ -10,40 +10,42 @@ TEST(lru_cache_test, basic)
     LruCache<int, std::string> c(4, 0);
 
     EXPECT_EQ(c.size(), 0);
-    EXPECT_EQ(c.get_size(), 0);
     EXPECT_TRUE(c.empty());
     EXPECT_EQ(c.lookup(100), nullptr);
 
     c.clear();
-    EXPECT_EQ(c.get_size(), 0);
+    EXPECT_EQ(c.size(), 0);
     c.purge();
-    EXPECT_EQ(c.get_size(), 0);
+    EXPECT_EQ(c.size(), 0);
     c.purge(2, 100);
-    EXPECT_EQ(c.get_size(), 0);
+    EXPECT_EQ(c.size(), 0);
 
     c.insert(100, "100");
     c.insert(101, "101");
     c.insert(102, "102");
-    EXPECT_EQ(c.get_size(), 3);
+    EXPECT_EQ(c.size(), 3);
     EXPECT_FALSE(c.empty());
 
     c.purge(2, 100);
-    EXPECT_EQ(c.get_size(), 2);
+    EXPECT_EQ(c.size(), 2);
     EXPECT_EQ(c.lookup(100), nullptr);
     EXPECT_STREQ(c.lookup(101)->c_str(), "101");
     EXPECT_STREQ(c.lookup(102)->c_str(), "102");
 
     c.purge(1, 100);
-    EXPECT_EQ(c.get_size(), 1);
+    EXPECT_EQ(c.size(), 1);
     EXPECT_EQ(c.lookup(101), nullptr);
 
     c.purge(0, 100);
-    EXPECT_EQ(c.get_size(), 0);
+    EXPECT_EQ(c.size(), 0);
     EXPECT_EQ(c.lookup(102), nullptr);
     EXPECT_TRUE(c.empty());
 
     c.insert(103, "103");
-    EXPECT_EQ(c.get_size(), 1);
+    EXPECT_EQ(c.size(), 1);
+
+    auto *ptr = c.insert(101, "xx");
+    EXPECT_EQ(*ptr, "xx");
 
     c.clear();
     EXPECT_TRUE(c.empty());
@@ -56,13 +58,13 @@ TEST(lru_cache_test, automatic_purge)
     c.insert(100, "100");
     c.insert(101, "101");
     c.insert(102, "102");
-    EXPECT_EQ(c.get_size(), 3);
+    EXPECT_EQ(c.size(), 3);
 
     c.insert(103, "103");
-    EXPECT_EQ(c.get_size(), 4);
+    EXPECT_EQ(c.size(), 4);
 
     c.insert(104, "104");
-    EXPECT_EQ(c.get_size(), 4);
+    EXPECT_EQ(c.size(), 4);
 }
 
 TEST(lru_cache_test, manual_purge)
@@ -74,13 +76,16 @@ TEST(lru_cache_test, manual_purge)
     c.insert(102, "102");
     c.insert(103, "103");
     c.insert(104, "104");
-    EXPECT_EQ(c.get_size(), 5);
+    EXPECT_EQ(c.size(), 5);
 
     c.insert(102, "xxxx");
-    EXPECT_EQ(c.get_size(), 5);
+    EXPECT_EQ(c.size(), 5);
 
     c.insert(109, "109");
-    EXPECT_EQ(c.get_size(), 6);
+    EXPECT_EQ(c.size(), 6);
+
+    EXPECT_EQ(*c.lookup(101), "101");
+    EXPECT_EQ(*c.lookup(102), "xxxx");
 }
 
 TEST(lru_cache_test, aging)
@@ -97,25 +102,25 @@ TEST(lru_cache_test, aging)
     c.insert(100, "100");
     c.insert(101, "101");
     c.insert(102, "102");
-    EXPECT_EQ(c.get_size(), 3);
+    EXPECT_EQ(c.size(), 3);
 
     c.advance_clock();
     c.insert(103, "103");
-    EXPECT_EQ(c.get_size(), 4);
+    EXPECT_EQ(c.size(), 4);
 
     c.advance_clock();
     c.insert(104, "104");
-    EXPECT_EQ(c.get_size(), 5);
+    EXPECT_EQ(c.size(), 5);
 
     // Max age 1 --> only the elems touched since
     // the second-to-last clock tick.
     c.purge(-1, 1);
-    EXPECT_EQ(c.get_size(), 2);
+    EXPECT_EQ(c.size(), 2);
 
     // Max age 0 --> only the elems touched since
     // the last clock tick.
     c.purge(-1, 0);
-    EXPECT_EQ(c.get_size(), 1);
+    EXPECT_EQ(c.size(), 1);
 
     c.advance_clock();
 
@@ -125,24 +130,75 @@ TEST(lru_cache_test, aging)
     EXPECT_TRUE(c.empty());
 }
 
-struct A
-{
-    A(int) {}
-    A(A &&) = default;
-    A &operator=(A &&) = default;
-    A(const A &) = delete;
-    A &operator==(const A &) = delete;
-};
-
-static_assert(std::is_move_constructible_v<A>);
-static_assert(!std::is_copy_constructible_v<A>);
-
 TEST(lru_cache_test, movable_only)
 {
+    struct A
+    {
+        A(int) {}
+        A(A &&) = default;
+        A &operator=(A &&) = default;
+        A(const A &) = delete;
+        A &operator=(const A &) = delete;
+    };
+
+    static_assert(std::is_move_constructible_v<A>);
+    static_assert(!std::is_copy_constructible_v<A>);
+
     LruCache<int, A> c;
     c.insert(0, A{123});
 
     A a{456};
     // c.insert(0, a); // fails to compile, can't copy.
     c.insert(0, std::move(a));
+}
+
+
+TEST(lru_cache_test, copyable_only)
+{
+    struct A
+    {
+        A(int) {}
+        A(A &&) = delete;
+        A &operator=(A &&) = delete;
+        A(const A &) = default;
+        A &operator=(const A &) = default;
+    };
+
+    static_assert(!std::is_move_constructible_v<A>);
+    static_assert(std::is_copy_constructible_v<A>);
+
+    LruCache<int, A> c;
+    c.insert(0, A{123});
+
+    A a{456};
+    c.insert(0, a);
+
+    // This does compile, b/c std::move doesn't actually move
+    // and when it comes time to call a constructor, we dispatch
+    // to copy or move c'tor based on whether Value implements those.
+    c.insert(0, std::move(a));
+}
+
+
+TEST(lru_cache_test, emplace)
+{
+    struct A
+    {
+        A(int i, std::string s, double d) : i(i), s(std::move(s)), d(d) {}
+        int i;
+        std::string s;
+        double d;
+    };
+
+    LruCache<int, A> c;
+    A *a1 = c.emplace(3, 1, "foo", 3.0);
+    A *a2 = c.emplace(2, 7, "bar", 17.0);
+    A *a3 = c.emplace(2, 3, "baz", 99.0);
+    A *a4 = c.emplace(6, 99, "bla", -13.0);
+
+    EXPECT_EQ(c.size(), 3);
+    EXPECT_EQ(a1->i, 1);
+    EXPECT_EQ(a2->i, 3);
+    EXPECT_EQ(a3->i, 3);
+    EXPECT_EQ(a4->i, 99);
 }
